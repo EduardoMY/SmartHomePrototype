68057bf379f0957cc3162990f37a881edf305140
Eduardo Medina <eduardo.medina@cetys.edu.mx>
2017-05-15 18:34:08 -0700

    Spotify functionally added : )

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+'use strict';
+
 var express = require('express')
 var app = express()
+var find = require('spotify-find')
+var connect = require('spotify-local-control')
+var client = connect()
+var gpio = require('rpi-gpio')
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -10,11 +16,32 @@ app.listen(3000, function () {
 })
 
 app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' /*&& req.query['hub.verify_token'] === <VERIFY_TOKEN> */) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
+    var action =  (req.query.param1).split(' ');
+    console.log(action)
+    console.log(req.query.param1)
+    doAction(action)
+    res.send([{ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..."}]);
 });
+
+function doAction(tokens){
+    if(doAction.length===0)
+	return ;
+    if(tokens[0]==="Play"){
+	tokens.shift();
+	var song = tokens.join(' ')
+	console.log(song)
+	find({q: song, type: 'track'}).then(function(res) {
+	    //console.log(res.tracks)
+	    client.play(res.tracks.items[0].uri)
+	})	
+    }
+    else if(tokens[0]==="Stop")
+	client.stop()
+    else if(tokens[0]==="Pause")
+	client.pause()
+    else if(tokens[0]==="Resume")
+	client.resume()
+    else
+	console.log('WHatDaFu')
+	
+}
