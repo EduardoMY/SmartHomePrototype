796550ac4136f5c6cfcef29bd1bf7efb613c013c
Eduardo Medina <eduardo.medina@cetys.edu.mx>
2017-05-21 14:28:22 -0700

    User agent added

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 'use strict';
 
+//Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36
+
+
 var express = require('express')
 var app = express()
 var find = require('spotify-find')
-var connect = require('spotify-local-control')
-var client = connect()
 var gpio = require('rpi-gpio')
 var https=require('https')
 
-var token = 'BQB-vecuScLUoJ6Fa07CIwgwxsZQ2n-1t1FLSpT9lR-GZVf5JrM7u-oNyhfe2IAqxm6pghyihofB7EY3ma1ug9GglLguPDUIIq0wZmyiTL3FaDEEgUqhnXazxaI4K3ySJ0Vr4RnUWEKP8W4T1bXr1NvITtY77LSaLaNLy8A9OLg77-g4N_jpS9pzH-9PvCiURWAy-DOKZSc9UaoWiz6QqOAyVrwTS6beiIRkhdnPOHPkcJjq6d2SAlHCWrLcZ-3BIZ78DElI2OVIV820_zvaNcd0MIoYYdB0welz1khUg9iUcKlb4LmzBlR_nk1WQEvakRGSSMaeOtU'
+var token = ''
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
-
-var options = {host:'api.spotify.com',
-	       path:'/v1/me/player/pause',
-	       method:'PUT',
-	       headers: {
-		   'Authorization': 'Bearer ' + token
-	       }};
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
@@ -29,13 +23,12 @@ app.get('/webhook', function(req, res) {
     var action =  (req.query.param1).split(' ');
     console.log(action)
     console.log(req.query.param1)
-    doAction(action)
-    res.send([{ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..."}]);
+    res.send([{ "text": doAction(action)}]);
 });
 
 function doAction(tokens){
     if(tokens.length===0)
-	return ;
+	return "Fuck Poo 0";
     if(tokens[0]==="Play"){
 	tokens.shift();
 	var song = tokens.join(' ')
@@ -43,7 +36,6 @@ function doAction(tokens){
 	find({q: song, type: 'track'}).then(function(res) {
 	    console.log(res.tracks)
 	    runHTTPRequest(getOptions(1), '{"uris": ["'+res.tracks.items[0].uri+'"]}')
-	    //client.play(res.tracks.items[0].uri)
 	});	
     }
     else if(tokens[0]=="Resume")
@@ -53,7 +45,9 @@ function doAction(tokens){
     else if(tokens[0]==="Next")
 	runHTTPRequest(getOptions(3), "")
     else
-	console.log('WHatDaFu')	
+	console.log('WHatDaFu')
+    
+    return "Fuck Poo"
 }
 
 function getOptions(action){
